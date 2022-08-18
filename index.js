ff3198e4a9e74b00e8c21ee7ed00fdd923e0970f
Kaldiris <kaldiris@gmail.com>
2022-08-18 17:28:24 -0300

    reconf

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const path = require('path')
 
 const formidable = require('formidable');
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
